c3d0e8006859fda3511dfcb4cb5ef9a014fdc6c2
hyeryun3 <hryun3@gmail.com>
2022-06-27 16:39:05 +0900

    테스트 코드 작성

--- src/test/java/com/triple/triplehomework/service/PointServiceTest.java
+++ src/test/java/com/triple/triplehomework/service/PointServiceTest.java
@@ -1,6 +1,7 @@
 package com.triple.triplehomework.service;
 
 import com.triple.triplehomework.domain.Place;
+import com.triple.triplehomework.domain.Point;
 import com.triple.triplehomework.domain.User;
 import com.triple.triplehomework.dto.PointDTO;
 import com.triple.triplehomework.dto.ReviewDTO;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -34,34 +36,75 @@ public class PointServiceTest {
     static String[] attachedPhotoIds = {"e4d1a64e-a531-46de-88d0-ff0ed70c0bb8", "afb0cef2-851d-4a50-bb07-9cc15cbdc332"};
 
     @Test
-    public void userPoint() {
+    public void plusPoint(){
         // given
         User user = new User(userUuid);
-        user = userRepository.save(user);
+        Place place = new Place(placeUuid);
+        String info = "포인트 추가";
+
+        // when
+        Point point = pointService.plusPoint(info,user,place);
+
+        // then
+        Assert.assertEquals("포인트는 1씩 적립된다.",1,point.getPoint());
+        Assert.assertEquals("포인트 적립 내용은 '포인트 추가'",info,point.getInfo());
+    }
+
+    @Test
+    public void minusPoint(){
+        // given
+        User user = new User(userUuid);
+        Place place = new Place(placeUuid);
+        String info = "포인트 차감";
+
+        // when
+        Point point = pointService.minusPoint(info,user,place);
+
+        // then
+        Assert.assertEquals("포인트는 1씩 차감된다.",-1,point.getPoint());
+        Assert.assertEquals("포인트 적립 내용은 '포인트 차감'",info,point.getInfo());
+    }
+
+    @Test
+    public void updatePoint(){
+        // given
+        User user = new User(userUuid);
+        userRepository.save(user);
 
         Place place = new Place(placeUuid);
         placeRepository.save(place);
 
-        ReviewDTO dto = new ReviewDTO();
-        dto.setUserId(userUuid);
-        dto.setPlaceId(placeUuid);
-        dto.setContent("리뷰작성");
-        dto.setReviewId(reviewUuid);
-        dto.setAttachedPhotoIds(attachedPhotoIds);
-        reviewService.addReview(dto);
+        String info = "포인트 추가";
+        for(int i=0; i<3; i++){
+            pointService.plusPoint(info,user,place);
+        }
+
+        // when
+        pointService.updatePoint(user);
+
+        // then
+        Assert.assertEquals("사용자의 포인트는 업데이트 되어야 한다.",3,userRepository.findByUserUuid(userUuid).getPoint());
+    }
+
+    @Test
+    public void userPoint(){
+        // given
+        User user = new User(userUuid);
+        userRepository.save(user);
 
         // when
         int point = pointService.userPoint(userUuid);
 
         // then
-        Assert.assertEquals(point, pointRepository.getSumOfPoint(user.getUserId()));
+        Assert.assertEquals("존재하지 않는 사용자면 -1을 리턴한다.",-1,pointService.userPoint("1212"));
+        Assert.assertEquals("사용자의 현재 포인트를 리턴한다.",0, point);
     }
 
     @Test
-    public void pointRecord() { // 수정해야함.
+    public void pointRecord(){
         // given
         User user = new User(userUuid);
-        user = userRepository.save(user);
+        userRepository.save(user);
 
         Place place = new Place(placeUuid);
         placeRepository.save(place);
@@ -72,12 +115,17 @@ public class PointServiceTest {
         dto.setContent("리뷰작성");
         dto.setReviewId(reviewUuid);
         dto.setAttachedPhotoIds(attachedPhotoIds);
+
         reviewService.addReview(dto);
 
         // when
         List<PointDTO> pointRecord = pointService.pointRecord(userUuid);
 
         // then
-        Assert.assertEquals(pointRecord, pointRepository.findByUser_UserId(user.getUserId()));
+        Assert.assertEquals("포인트 기록_포인트",1, pointRecord.get(0).getPoint());
+        Assert.assertEquals("포인트 기록_내용","리뷰 작성_텍스트 작성 포인트 부여", pointRecord.get(0).getInfo());
+        Assert.assertEquals("포인트 기록_내용","리뷰 작성_사진 첨부 포인트 부여", pointRecord.get(1).getInfo());
+        Assert.assertEquals("포인트 기록_내용","리뷰 작성_특정 장소 첫 리뷰 포인트 부여", pointRecord.get(2).getInfo());
+        Assert.assertEquals("포인트 기록_날짜", LocalDate.now(), pointRecord.get(0).getDate());
     }
 }
